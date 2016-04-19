21b1ffc2455703de887eca43b537c995b1d4cfc9
miaochuanhai <miaochuanhai@163.com>
2016-04-19 16:17:06 +0800

    111

--- app/src/main/java/com/atguigu/gittest01/MainActivity.java
+++ app/src/main/java/com/atguigu/gittest01/MainActivity.java
@@ -2,6 +2,7 @@ package com.atguigu.gittest01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("TAG", "111111111111");
+        Log.e("TAG", "222222222");
     }
 }
